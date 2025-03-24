575de7b6fd54234e9fd16738d0959df04760f662
RONCELLI.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-03-24 12:31:24 +0100

    Controllo relativo all'esistenza del file.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[]args){
         String nomeFile= "roncelli.csv";
-        String linea="";
-        try(BufferedReader br=new BufferedReader(new FileReader("roncelli.csv"))){
-            linea=br.readLine();
-            while(linea!=null){
-                System.out.println(linea);
-                linea=br.readLine();
+        File file=new File(nomeFile);
+        if(file.exists()) {
+            String linea = "";
+            try (BufferedReader br = new BufferedReader(new FileReader("roncelli.csv"))) {
+                linea = br.readLine();
+                while (linea != null) {
+                    System.out.println(linea);
+                    linea = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }catch(IOException e){
-            e.printStackTrace();
-        };
+        }else{
+            System.err.println("File inesistente!");
+        }
     }
 }

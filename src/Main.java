4b32f6bb90495931cd5a1d2d4876a7207fd05625
RONCELLI.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-03-24 12:27:04 +0100

    Stampa del contenuto del file.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[]args){
+        String nomeFile= "roncelli.csv";
+        String linea="";
+        try(BufferedReader br=new BufferedReader(new FileReader("roncelli.csv"))){
+            linea=br.readLine();
+            while(linea!=null){
+                System.out.println(linea);
+                linea=br.readLine();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        };
+    }
 }
